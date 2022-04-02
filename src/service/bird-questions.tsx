7620ff4e483c4dce6488ff7d719fad88b3f34ad1
kblanchfield <kateblanchfield@live.co.uk>
2022-04-02 20:35:22 +0200

    Update response body in front-end

--- src/service/bird-questions.tsx
+++ src/service/bird-questions.tsx
@@ -1,26 +1,23 @@
-import { apiRequest } from './fetch'
-import { IQuestion } from '../models'
+import { apiRequest } from "./fetch"
+import { IQuestion } from "../models"
 
 const mapQuestions = (questions: any[]): IQuestion[] => {
-    return questions.map((question: any): IQuestion => {
-        return {
-            answer: question.answer,
-            answerOptions: question.answer_options,
-            bird: question.bird,
-        }
-    })
+  return questions.map((question: any): IQuestion => {
+    return {
+      answer: question.answer,
+      answerOptions: question.answer_options,
+      bird: question.bird,
+    }
+  })
 }
 
 export const fetchQuestions = async (): Promise<IQuestion[]> => {
-    try {
-        const response = await apiRequest(
-            "/.netlify/functions/bird-questions/",
-            "get"
-        )
+  try {
+    const response = await apiRequest("/.netlify/functions/questions/", "get")
 
-        return mapQuestions(response.birdQuestions)
-    } catch (err) {
-        console.log('Error getting bird questions: ', err)
-        return []
-    }
+    return mapQuestions(response)
+  } catch (err) {
+    console.log("Error getting bird questions: ", err)
+    return []
+  }
 }
--- src/service/fetch.tsx
+++ src/service/fetch.tsx
@@ -1,12 +1,13 @@
-import { IQuestion } from "../models"
-
-export const apiRequest = async (url: string, method: string): Promise<{ birdQuestions: IQuestion[] }> => {
-    const response = await fetch(url, {
-      method,
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json"
-      }
-    })
-    return response.json()
-  }
+export const apiRequest = async (
+  url: string,
+  method: string
+): Promise<any[]> => {
+  const response = await fetch(url, {
+    method,
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+  })
+  return response.json()
+}
